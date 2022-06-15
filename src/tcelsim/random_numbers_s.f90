0c7e40f310a80dc936968f16ca578a4d150c0a40
Dominick <Dominick Martinez>
2022-06-14 19:53:36 -0600

    refactor(random_numbers_m): create rand_num_table_ subroutine

---
+++ src/tcelsim/random_numbers_s.f90
@@ -0,0 +1,21 @@
+submodule(random_numbers_m) random_numbers_s
+  implicit none
+  
+contains
+
+  module procedure create_rand_num_table
+  
+    ! Local variables
+    integer i,j,k
+    
+    do i = 1,ncells
+      do j = 1,npositions
+        do k = 1,4
+          call random_number(random_number_table(i,j,k))
+        end do
+      end do
+    end do
+    
+  end procedure create_rand_num_table
+  
+end submodule random_numbers_s
