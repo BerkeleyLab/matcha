2428d9bdabd30e50ba5b5c12e5fad84c4b596cf4
davytorres <davytorres@nnmc.edu>
2022-06-16 16:34:03 -0600

    Created random number table for initial cell positions

--- app/main.f90
+++ app/main.f90
@@ -2,24 +2,28 @@ program tcell
   use tcelsim, only : initialize_positions, create_distribution, move_tcells
   implicit none
 
-  integer ncells, npositions, nintervals, nveldim
-  parameter(ncells = 100, npositions = 25, nveldim = 4)
+  integer ncells, npositions, nintervals, nveldim, ndim
+  parameter(ncells = 100, npositions = 25, nveldim = 4, ndim = 3)
   parameter(nintervals = 10)
 
   double precision vel(nintervals)
   double precision cumulative_distribution(0:nintervals)
 
   double precision, allocatable :: random_number_table(:,:,:)
+  double precision, allocatable :: random_number_table_positions(:,:)
   double precision, allocatable :: x(:,:)
   double precision, allocatable :: y(:,:)
   double precision, allocatable :: z(:,:)
   
   allocate(random_number_table(ncells,npositions,nveldim))
+  allocate(random_number_table_positions(ncells,ndim))
   allocate(x(ncells,npositions))
   allocate(y(ncells,npositions))
   allocate(z(ncells,npositions))
 
-  call initialize_positions(x,y,z,ncells,npositions)
+  call random_number(random_number_table_positions)
+
+  call initialize_positions(x,y,z,random_number_table_positions,ncells,npositions,ndim)
 
   call create_distribution(vel,cumulative_distribution,nintervals)
   
--- src/tcelsim/t_cell_m.f90
+++ src/tcelsim/t_cell_m.f90
@@ -3,9 +3,10 @@ module t_cell_m
 
   interface   
     
-    module subroutine initialize_positions(x,y,z,ncells,npositions)
+    module subroutine initialize_positions(x,y,z,random_number_table_positions,ncells,npositions,ndim)
       implicit none
-      integer, intent(in) :: ncells,npositions
+      integer, intent(in) :: ncells,npositions,ndim
+      double precision, intent(in)  :: random_number_table_positions(ncells,ndim)
       double precision, intent(out) :: x(ncells,npositions)
       double precision, intent(out) :: y(ncells,npositions)
       double precision, intent(out) :: z(ncells,npositions)
