c18a72c541eb99c45b7b90aeac5679c942c9fde5
Damian Rouson <rouson@lbl.gov>
2022-06-16 17:01:56 -0700

    refactor(main/t_cell_{m,s}): assumed-shape arrays
    This commit converts dummy arguments in the create_distributions
    subroutine from explicit-shape to assumed-shape.

--- src/tcelsim/t_cell_m.f90
+++ src/tcelsim/t_cell_m.f90
@@ -3,14 +3,11 @@ module t_cell_m
 
   interface   
     
-    module subroutine initialize_positions(x,y,z,random_number_table_positions,ncells,npositions,ndim)
+    module subroutine initialize_positions(x,y,z,random_positions)
       implicit none
-      integer, intent(in) :: ncells,npositions,ndim
-      double precision, intent(in)  :: random_number_table_positions(ncells,ndim)
-      double precision, intent(out) :: x(ncells,npositions)
-      double precision, intent(out) :: y(ncells,npositions)
-      double precision, intent(out) :: z(ncells,npositions)
-    end subroutine initialize_positions
+      double precision, intent(in)  :: random_positions(:,:)
+      double precision, intent(out) :: x(:), y(:), z(:)
+    end subroutine
     
   end interface
 
