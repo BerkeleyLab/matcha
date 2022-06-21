ec757c908af985c23277dc323b1142e186ce8f9f
Damian Rouson <rouson@lbl.gov>
2022-06-21 11:18:15 -0700

    refactor(t_cell_s): switch array section -> whole array

--- src/tcelsim/t_cell_s.f90
+++ src/tcelsim/t_cell_s.f90
@@ -9,9 +9,9 @@ contains
     double precision, parameter :: scaling_factor  = 100.
 
     !     Assign initial positions to T cells randomly in a [100x100x100] grid
-    x(:) = random_positions(:,1)*scaling_factor
-    y(:) = random_positions(:,2)*scaling_factor
-    z(:) = random_positions(:,3)*scaling_factor
+    x = random_positions(:,1)*scaling_factor
+    y = random_positions(:,2)*scaling_factor
+    z = random_positions(:,3)*scaling_factor
 
   end procedure initialize_positions
 
