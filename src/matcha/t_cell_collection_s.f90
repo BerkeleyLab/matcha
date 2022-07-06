5d7a05a0ae12ad886f8ed74f1c9c9f6973f28e03
Dominick99 <106998745+Dominick99@users.noreply.github.com>
2022-07-06 12:02:57 -0600

    Update src/matcha/t_cell_collection_s.f90
    Co-authored-by: Damian Rouson <rouson@lbl.gov>

--- src/matcha/t_cell_collection_s.f90
+++ src/matcha/t_cell_collection_s.f90
@@ -11,7 +11,6 @@ contains
   
   module procedure positions
     call assert(allocated(self%positions_), "t_cell_collection_t%positions: allocated(positions_)")
-      "t_cell_collection_t%positions: allocated(positions_)")
     my_positions = self%positions_
   end procedure
   
