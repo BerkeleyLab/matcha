d6e08569c55895ba621b3101c00ce254cd3e6037
Damian Rouson <rouson@lbl.gov>
2022-07-09 22:24:31 -0700

    refac(matcha function): white space edits

--- src/matcha_m.f90
+++ src/matcha_m.f90
@@ -17,8 +17,6 @@ module matcha_m
 contains
 
   module procedure matcha
-    double precision, parameter :: scale = 100.D0
-    double precision, allocatable :: random_positions(:,:)
 
     associate( &
       ncells => input%num_cells(), &
@@ -27,37 +25,37 @@ contains
       nintervals => input%num_intervals(), &
       dt => input%time_step() &
     )
-    associate(nsteps => npositions -1)
 
-    allocate(random_positions(ncells,ndim))
-    call random_number(random_positions)  
-    history = [t_cell_collection_t(scale*random_positions, time=0.D0)]
+      block
+        double precision, parameter :: scale = 100.D0
+        double precision, allocatable :: sample_distribution(:), random_positions(:,:), random_4vectors(:,:,:), velocities(:,:,:)
+        type(distribution_t) distribution
+        integer, parameter :: nveldim = 4
+        integer step
 
-    block
-      integer, parameter :: nveldim = 4
-      double precision, allocatable :: random_4vectors(:,:,:), sample_distribution(:), velocities(:,:,:)
-      type(distribution_t) distribution
-      integer step
-      
-      allocate(random_4vectors(ncells,nsteps,nveldim))
-      call random_number(random_4vectors)  
-      allocate(sample_distribution(nintervals))
-      call random_number(sample_distribution)
-      sample_distribution = sample_distribution/sum(sample_distribution)
-      distribution = distribution_t(sample_distribution)
-      
-      associate(random_speeds => random_4vectors(:,:,1), random_directions => random_4vectors(:,:,2:4))
-        associate(v => distribution%velocities(random_speeds, random_directions))
-          do step = 1, nsteps
-            associate(x => history(step)%positions(), t => history(step)%time())
-              history = [history, t_cell_collection_t(x + v(:,step,:)*dt, t + dt)]
+        allocate(random_positions(ncells,ndim))
+        call random_number(random_positions)  
+        allocate(sample_distribution(nintervals))
+        call random_number(sample_distribution)
+          
+        associate(nsteps => npositions -1)
+          allocate(random_4vectors(ncells,nsteps,nveldim))
+          call random_number(random_4vectors)  
+          sample_distribution = sample_distribution/sum(sample_distribution)
+          distribution = distribution_t(sample_distribution)
+          
+          associate(random_speeds => random_4vectors(:,:,1), random_directions => random_4vectors(:,:,2:4))
+            associate(v => distribution%velocities(random_speeds, random_directions))
+              history = [t_cell_collection_t(scale*random_positions, time=0.D0)]
+              do step = 1, nsteps
+                associate(x => history(step)%positions(), t => history(step)%time())
+                  history = [history, t_cell_collection_t(x + v(:,step,:)*dt, t + dt)]
+                end associate
+              end do
             end associate
-          end do
+          end associate
         end associate
-      end associate
-    end block
-
-    end associate
+      end block
     end associate
 
   end procedure
