453908d5b32d1afbf602f11b37ae703052b9de9a
Jerrymiah12 <Jerrymiah12@gmail.com>
2022-07-11 13:31:54 -0700

    fix

--- app/matcha.f90
+++ app/matcha.f90
@@ -8,7 +8,7 @@ program matcha_app
   type(data_partition_t) :: data_partition
   integer a, b
 
-  call data_partition%define_partitions(cardinality=input%ncells)
+  call data_partition%define_partitions(cardinality=input%ncells())
   input%num_cells_ = data_partition%last(this_image()) - data_partition%first(this_image())
 
 
