e91ca8a62cbc617f2cc8b5586d388ffb09dd2a0b
Damian Rouson <rouson@lbl.gov>
2022-08-02 00:00:49 -0700

    chore: rename matcha app to main

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,6 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
-program matcha_app
+program main
   !! Matcha: Motility Analysis of T-Cell Histories in Activation
   use matcha_m, only : matcha, input_t, output_t
   implicit none
