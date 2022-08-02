37e8958a264caea8b450645217d04a31644b6587
Brad Richardson <everythingfunctional@protonmail.com>
2022-08-02 17:28:06 -0500

    feat: allow running with caffeine

--- app/main.F90
+++ app/main.F90
@@ -3,8 +3,15 @@
 program main
   !! Matcha: Motility Analysis of T-Cell Histories in Activation
   use matcha_m, only : matcha, input_t, output_t
+#ifdef USE_CAFFEINE
+   use caffeine_m, only : caf_caffeinate, caf_decaffeinate
+#endif
   implicit none
   type(output_t) output
+
+#ifdef USE_CAFFEINE
+  if (caf_caffeinate() /= 0) stop
+#endif
 
   associate(input => input_t())
     output = output_t(input, matcha(input))
@@ -13,4 +20,7 @@ program main
   print *
   print *,"----> Matcha done. <----"
 
+#ifdef USE_CAFFEINE
+  call caf_decaffeinate(0)
+#endif
 end program
