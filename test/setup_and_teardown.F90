2afb26a1bb29133d8a4028cbff00a390b06a4619
Damian Rouson <rouson@lbl.gov>
2022-08-03 14:32:20 -0700

    fix(test): call caf_error_stop

--- test/setup_and_teardown.F90
+++ test/setup_and_teardown.F90
@@ -1,12 +1,14 @@
 module setup_and_teardown
 #ifdef USE_CAFFEINE
-    use caffeine_m, only : caf_caffeinate, caf_decaffeinate
+    use caffeine_m, only : caf_caffeinate, caf_decaffeinate, caf_error_stop
 #endif
     implicit none
 contains
     subroutine setup
 #ifdef USE_CAFFEINE
-        if (caf_caffeinate() /= 0) stop
+        associate(stop_code => caf_caffeinate())
+          if (stop_code /= 0) call caf_error_stop(stop_code)
+        end associate
 #endif
     end subroutine
 
