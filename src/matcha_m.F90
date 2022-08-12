d7f85643c6e541bb24a0fd12b8f8da194fc82f70
Damian Rouson <rouson@lbl.gov>
2022-08-12 15:51:06 -0700

    doc(ford): work around ford issu 446
    Move the matcha function procedure to a new submodule.

--- src/matcha_m.F90
+++ src/matcha_m.F90
@@ -7,10 +7,6 @@ module matcha_m
   use output_m, only : output_t
   use data_partition_m, only : data_partition_t
   
-#ifdef USE_CAFFEINE
-   use caffeine_m, only : this_image => caf_this_image
-#endif
-
   implicit none
 
   interface matcha
@@ -23,58 +19,4 @@ module matcha_m
 
   end interface
   
-contains
-
-  module procedure matcha
-
-    associate( &
-      ncells => input%num_cells(), &
-      npositions => input%num_positions(), &
-      ndim => input%num_dimensions(), &
-      nintervals => input%num_intervals(), &
-      dt => input%time_step(), &
-      empirical_distribution => input%sample_distribution() &
-    )
-
-      block
-        double precision, parameter :: scale = 100.D0
-        double precision, allocatable :: random_positions(:,:), random_4vectors(:,:,:)
-        type(distribution_t) distribution
-        integer, parameter :: nveldim = 4
-        integer step
-        type(data_partition_t) data_partition
-        
-        call data_partition%define_partitions(cardinality=ncells)
-    
-        associate(me => this_image())
-          associate(my_num_cells => data_partition%last(me) - data_partition%first(me) + 1)
-          
-            call random_init(repeatable=.true., image_distinct=.true.)
-            
-            allocate(random_positions(my_num_cells,ndim))
-            call random_number(random_positions)  
-          
-            associate(nsteps => npositions -1)
-              allocate(random_4vectors(my_num_cells,nsteps,nveldim))
-              call random_number(random_4vectors)  
-              distribution = distribution_t(empirical_distribution)
-          
-              associate(random_speeds => random_4vectors(:,:,1), random_directions => random_4vectors(:,:,2:4))
-                associate(v => distribution%velocities(random_speeds, random_directions))
-                  history = [t_cell_collection_t(scale*random_positions, time=0.D0)]
-                  do step = 1, nsteps
-                    associate(x => history(step)%positions(), t => history(step)%time())
-                      history = [history, t_cell_collection_t(x + v(:,step,:)*dt, t + dt)]
-                    end associate
-                  end do
-                end associate
-              end associate
-            end associate
-          end associate  
-        end associate    
-      end block
-    end associate
-
-  end procedure
-
 end module
