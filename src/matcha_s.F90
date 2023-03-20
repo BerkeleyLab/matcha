580e34186f419a24ac72467478516d4367868ca1
davytorres <davytorreslight@gmail.com>
2023-03-20 11:28:09 -0600

    Added grid and gradient for regular communication between images

--- src/matcha_s.F90
+++ src/matcha_s.F90
@@ -3,8 +3,8 @@
 submodule(matcha_m) matcha_s
   use t_cell_collection_m, only : t_cell_collection_t
   use distribution_m, only : distribution_t
+  use gridr_m, only : gridr_t
   use data_partition_m, only : data_partition_t
-  
 #ifdef USE_CAFFEINE
    use caffeine_m, only : this_image => caf_this_image
 #endif
@@ -14,23 +14,42 @@ submodule(matcha_m) matcha_s
 contains
 
   module procedure matcha
-
+  
     associate( &
       ncells => input%num_cells(), &
       npositions => input%num_positions(), &
       ndim => input%num_dimensions(), &
       nintervals => input%num_intervals(), &
+      num_grid => input%ngrid(), &
       dt => input%time_step(), &
+      gb => input%grid_begin(), &
+      ge => input%grid_end(), &
+      cytokinev => input%cytokine(), &
+      gfacv => input%gfac(), &
       empirical_distribution => input%sample_distribution() &
-    )
-
+      )
+      
       block
         double precision, parameter :: scale = 100.D0
         double precision, allocatable :: random_positions(:,:), random_4vectors(:,:,:)
+        double precision, allocatable :: gdx(:)
+        double precision gxs
         type(distribution_t) distribution
+        type(gridr_t) gridr
+        integer, allocatable :: ng(:)
         integer, parameter :: nveldim = 4
+        integer i,j,k,ii,jj,kk
         integer step
+        double precision tconc
         type(data_partition_t) data_partition
+
+        allocate(ng(ndim))
+        allocate(gdx(2*ndim+1))
+        ng(:) = num_grid
+        gridr = gridr_t()
+        gdx = gridr%gridparameters(gb,ge,ng)
+        tconc = cytokinev*gfacv*gdx(7)
+
         
         call data_partition%define_partitions(cardinality=ncells)
     
@@ -38,7 +57,6 @@ contains
           associate(my_num_cells => data_partition%last(me) - data_partition%first(me) + 1)
           
             call random_init(repeatable=.true., image_distinct=.true.)
-            
             allocate(random_positions(my_num_cells,ndim))
             call random_number(random_positions)  
           
@@ -52,8 +70,11 @@ contains
                   allocate(history(nsteps))
                   history(1) = t_cell_collection_t(scale*random_positions, time=0.D0)
                   do step = 2, nsteps
-                    associate(x => history(step-1)%positions(), t => history(step-1)%time())
-                      history(step) = t_cell_collection_t(x + v(:,step-1,:)*dt, t + dt)
+                     associate(x => history(step-1)%positions(), t => history(step-1)%time())
+                       associate(gx => gridr%gradient(my_num_cells,ng,gdx,gb,tconc,x))  
+                         history(step) = t_cell_collection_t(x + (v(:,step-1,:) + gx(:,:))*dt, t + dt)
+                       end associate
+!                       history(step) = t_cell_collection_t(x + v(:,step-1,:)*dt, t + dt)
                     end associate
                   end do
                 end associate
