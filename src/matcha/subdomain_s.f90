5972c973ef5017e6922f4f0a87c24a9104d15198
Damian Rouson <rouson@lbl.gov>
2023-04-19 22:46:36 -0400

    feat(subdomain_m):partition grid along x direction

--- src/matcha/subdomain_s.f90
+++ src/matcha/subdomain_s.f90
@@ -1,17 +1,26 @@
 submodule(subdomain_m) subdomain_s
+  use data_partition_m, only : data_partition_t
   implicit none
 
   real, allocatable :: halo_x(:,:)[:]
   integer, parameter :: west=1, east=2
 
+  type(data_partition_t) data_partition
+
 contains
 
     module procedure define
-      associate(nx => (n), ny => (n), num_subdomains => num_images())
+
+      associate(nx => (n), ny => (n), num_subdomains => num_images(), me => this_image())
+
         allocate(halo_x(ny,west:east)[*])
-        allocate(self%s_(nx/num_subdomains, ny), source=0.)
-        self%dx_ = side/num_subdomains
-        self%dy_ = self%dx_
+        call data_partition%define_partitions(nx)
+
+        associate(my_first => data_partition%first(me), my_last => data_partition%last(me))
+          allocate(self%s_(my_first:my_last, ny), source=0.)
+          self%dx_ = side/num_subdomains
+          self%dy_ = self%dx_
+        end associate
       end associate
     end procedure
 
