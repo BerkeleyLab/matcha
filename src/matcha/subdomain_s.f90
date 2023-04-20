73794ecd499ed23cd93d411a3ddf5fc5c078060c
Damian Rouson <rouson@lbl.gov>
2023-04-20 05:19:31 -0400

    fix(subdomain_t%define): initialize subdomain

--- src/matcha/subdomain_s.f90
+++ src/matcha/subdomain_s.f90
@@ -1,5 +1,7 @@
 submodule(subdomain_m) subdomain_s
   use data_partition_m, only : data_partition_t
+  use assert_m, only : assert
+  use intrinsic_array_m, only : intrinsic_array_t
   implicit none
 
   real, allocatable :: halo_x(:,:)[:]
@@ -13,11 +15,24 @@ contains
 
       associate(nx => (n), ny => (n), num_subdomains => num_images(), me => this_image())
 
+        call assert(num_subdomains <= nx, "subdomain_t%define: num_subdomains <= nx", intrinsic_array_t([nx, num_subdomains]))
+
         allocate(halo_x(ny,west:east)[*])
         call data_partition%define_partitions(nx)
 
         associate(my_first => data_partition%first(me), my_last => data_partition%last(me))
-          allocate(self%s_(my_first:my_last, ny), source=0.)
+          allocate(self%s_(my_first:my_last, ny))
+          block
+            integer i
+
+            self%s_(my_first:my_last, 1) = boundary_val
+            do concurrent(i=2:ny-1)
+              self%s_(my_first, i) = merge(boundary_val, internal_val, my_first==1)
+              self%s_(my_first+1:my_last-1, i) = internal_val
+              self%s_(my_last, i) = merge(boundary_val, internal_val, my_last==nx)
+            end do
+            self%s_(my_first:my_last, ny) = boundary_val
+          end block
           self%dx_ = side/num_subdomains
           self%dy_ = self%dx_
         end associate
