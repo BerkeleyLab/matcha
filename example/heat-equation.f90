eb720b8010bfb2660e4be445ae73a30f0e9ef1c5
Damian Rouson <rouson@lbl.gov>
2023-04-21 01:35:15 -0700

    refac(example): improve nomenclature

--- example/heat-equation.f90
+++ example/heat-equation.f90
@@ -2,21 +2,19 @@ program main
   use subdomain_m, only : subdomain_t
   implicit none
 
-  integer, parameter :: nt = 50
-  real, parameter :: sigma = 0.25, nu = 0.05
-  type(subdomain_t) u
+  integer, parameter :: steps = 50
+  real, parameter :: alpha = 0.25
+  type(subdomain_t) T
+  integer step
 
-  call u%define(side=2., boundary_val=1., internal_val=2., n=31)
+  call T%define(side=1., boundary_val=0., internal_val=1., n=11) ! internally constant subdomain with a step down at the edges
 
-  associate(dx => u%dx(), dy => u%dy())
-    associate(dt => sigma * dx * dy / nu)
-      block
-        integer i
-
-        do i = 1, nt
-          u =  u + dt * nu * .laplacian. u
-        end do
-      end block
+  associate(dx => T%dx(), dy => T%dy())
+    associate(dt => 0.1 * alpha * dx * dy / 2)
+      do step = 1, steps
+        T =  T + dt * alpha * .laplacian. T
+      end do
     end associate
   end associate
+
 end program
