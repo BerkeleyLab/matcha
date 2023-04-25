2f0b4e818c942fed158076c43003f5de719589ac
Damian Rouson <rouson@lbl.gov>
2023-04-25 09:47:02 -0700

    test: start switch from garden to sourcery

--- test/main.f90
+++ test/main.f90
@@ -2,13 +2,16 @@
 ! Terms of use are as specified in LICENSE.txt
 program main
   use subdomain_test_m, only : subdomain_test_t
+  use t_cell_collection_test_m, only : t_cell_collection_test_t
   implicit none
 
   type(subdomain_test_t) subdomain_test
+  type(t_cell_collection_test_t) t_cell_collection_test
 
   integer :: passes=0, tests=0
 
   call subdomain_test%report(passes, tests)
+  call t_cell_collection_test%report(passes, tests)
 
   print *
   print '(*(a,:,g0))',"_________ In total, ",passes," of ",tests, " tests pass. _________"
