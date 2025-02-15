7816b87af8fc8f0fb9e5d572ff753ec24212c791
Damian Rouson <damian@rouson.net>
2025-02-15 13:33:22 -0800

    repro(test): manually in-line test function body

--- test/main.f90
+++ test/main.f90
@@ -3,21 +3,13 @@
   use matcha_m, only : matcha
   implicit none
   logical test_passes
-  test_passes = check_cell_distribution()
+  integer cell_collection_size 
+  type(t_cell_collection_t), allocatable :: history(:)
 
-contains
-
-  function check_cell_distribution() result(test_passes)
-    logical test_passes
-    integer cell_collection_size 
-    type(t_cell_collection_t), allocatable :: history(:)
-
-    associate(input => input_t())
-      history = matcha(input)
-      cell_collection_size = size(history(1)%positions(), 1)
-      call co_sum(cell_collection_size)
-      test_passes = input%num_cells() .eq. cell_collection_size
-    end associate
-  end function
-
+  associate(input => input_t())
+    history = matcha(input)
+    cell_collection_size = size(history(1)%positions(), 1)
+    call co_sum(cell_collection_size)
+    test_passes = input%num_cells() .eq. cell_collection_size
+  end associate
 end
