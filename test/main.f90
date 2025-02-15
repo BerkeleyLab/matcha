ab5012eb6d212a675ee0b8af1855ac483184fdd1
Damian Rouson <damian@rouson.net>
2025-02-15 13:25:17 -0800

    repro(test): rm unused function

--- test/main.f90
+++ test/main.f90
@@ -9,35 +9,9 @@ contains
 
   function results() result(test_results)
     type(test_result_t), allocatable :: test_results(:)
-
-    character(len=*), parameter :: longest_description = "is constructed with positions in the specified domain" 
-
-    test_results = test_result_t( &
-      [ character(len=len(longest_description)) :: &
-        "is constructed with positions in the specified domain", &
-        "distributes cells across images" &
-      ], &
-      [  check_constructed_domain(), &
-         check_cell_distribution() &
-       ] &
-    )
+    test_results = test_result_t(["distributes cells across images"], [check_cell_distribution()])
   end function
 
-  function check_constructed_domain() result(test_passes)
-    logical test_passes
-    integer, parameter :: ncells = 100, ndim = 3 
-    double precision random_positions(ncells,ndim)
-    double precision, parameter :: scale_factor=100.D0
-    type(t_cell_collection_t) t_cell_collection
-
-    call random_number(random_positions)    
-    t_cell_collection = t_cell_collection_t(scale_factor*random_positions,time=0.D0)
-         
-    associate(constructed_positions => t_cell_collection%positions())
-      test_passes = all(0.D0 <=  constructed_positions .and. constructed_positions <= scale_factor)
-    end associate
-  end function
-  
   function check_cell_distribution() result(test_passes)
     logical test_passes
     integer cell_collection_size 
