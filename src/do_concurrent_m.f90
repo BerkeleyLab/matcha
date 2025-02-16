24262cc9dc906ddfe841d51d0fce213b2a88e690
Damian Rouson <damian@rouson.net>
2025-02-15 18:58:09 -0800

    repro(do_concurrent_m): rm unused functions
    do_concurrent_speeds
    do_concurrent_k
    do_concurrent_output_distribution

--- src/do_concurrent_m.f90
+++ src/do_concurrent_m.f90
@@ -1,10 +1,9 @@
 module do_concurrent_m
   use iso_c_binding, only : c_double, c_int
-  use t_cell_collection_m, only : t_cell_collection_t, t_cell_collection_bind_C_t
+  use t_cell_collection_m, only : t_cell_collection_bind_C_t
   implicit none
   private
-  public :: do_concurrent_sampled_speeds, do_concurrent_my_velocities, do_concurrent_k, do_concurrent_speeds
-  public :: do_concurrent_output_distribution
+  public :: do_concurrent_sampled_speeds, do_concurrent_my_velocities
   
   interface
     
@@ -21,26 +20,6 @@ module do_concurrent_m
       real(c_double), intent(out), allocatable :: my_velocities(:,:,:)
     end subroutine
     
-    pure module subroutine do_concurrent_k(speeds, vel, k) bind(C)
-      implicit none
-      real(c_double), intent(in) :: speeds(:), vel(:)
-      integer(c_int), intent(out), allocatable :: k(:)
-    end subroutine
-    
-    pure module subroutine &
-      do_concurrent_output_distribution(nintervals, speed, freq, emp_distribution, k, output_distribution) bind(C)
-      implicit none
-      integer(c_int), intent(in) :: nintervals, speed, freq, k(:)
-      real(c_double), intent(in) :: emp_distribution(:,:)
-      real(c_double), intent(out), allocatable :: output_distribution(:,:)
-    end subroutine
-    
-    module subroutine do_concurrent_speeds(history, speeds) bind(C)
-      implicit none
-      type(t_cell_collection_bind_C_t), intent(in) :: history(:)
-      real(c_double), intent(out), allocatable :: speeds(:)
-    end subroutine
-      
   end interface
   
 end module do_concurrent_m
