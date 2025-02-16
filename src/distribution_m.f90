dc6832adfb87c79f33494d2f72b574bb92bb2bfe
Damian Rouson <damian@rouson.net>
2025-02-15 19:13:26 -0800

    repro(distribution_{m,s}): combine

--- src/distribution_m.f90
+++ src/distribution_m.f90
@@ -1,10 +1,6 @@
-! Copyright (c), The Regents of the University of California
-! Terms of use are as specified in LICENSE.txt
 module distribution_m
+  use do_concurrent_m, only : do_concurrent_sampled_speeds, do_concurrent_my_velocities
   implicit none
-  
-  private
-  public :: distribution_t
   
   type distribution_t
     private
@@ -41,5 +37,48 @@ module distribution_m
     end function velocities
 
   end interface
+
+contains
+  
+  pure function monotonically_increasing(f) result(monotonic)
+    double precision, intent(in) :: f(:)
+    logical monotonic
+    integer i
+    monotonic = all([(f(i+1) >= f(i), i=1, size(f)-1)])
+  end function
+
+  module procedure construct
+    integer i
+    if (.not. all(sample_distribution(:,2)>=0.D0)) error stop "negative sample_distribution value(s)"
+    associate(nintervals => size(sample_distribution,1))      
+      distribution%vel_ = [(sample_distribution(i,1), i =1, nintervals)]  ! Assign speeds to each distribution bin         
+      distribution%cumulative_distribution_ = [0.D0, [(sum(sample_distribution(1:i,2)), i=1, nintervals)]]
+      if (.not. monotonically_increasing(distribution%cumulative_distribution_)) error stop "non-monotonic cum dist"
+    end associate
+  end procedure construct
+
+  module procedure cumulative_distribution
+    if (.not. allocated(self%cumulative_distribution_)) error stop "unallocatd cum dist"
+    my_cumulative_distribution = self%cumulative_distribution_
+  end procedure 
+  
+  module procedure velocities
+    double precision, allocatable :: sampled_speeds(:,:),  dir(:,:,:)
+    
+    if (.not. allocated(self%cumulative_distribution_)) error stop "unallocatd cum dist"
+    if (.not. allocated(self%vel_)) error stop "unallocated vel_"
+    call do_concurrent_sampled_speeds(speeds, self%vel_, self%cumulative_distribution(), sampled_speeds)
+    associate(nsteps => size(speeds,2))
+      dir = directions(:,1:nsteps,:)
+      associate(dir_mag => sqrt(dir(:,:,1)**2 +dir(:,:,2)**2 + dir(:,:,3)**2))
+        associate(dir_mag_ => merge(dir_mag, epsilon(dir_mag), dir_mag/=0.))
+          dir(:,:,1) = dir(:,:,1)/dir_mag_
+          dir(:,:,2) = dir(:,:,2)/dir_mag_
+          dir(:,:,3) = dir(:,:,3)/dir_mag_
+        end associate
+      end associate
+      call do_concurrent_my_velocities(nsteps, dir, sampled_speeds, my_velocities)
+    end associate
+  end procedure
 
 end module distribution_m
